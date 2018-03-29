0baf67204ad4dd7096d2fce2e3a068e224766206
Marcus Ackland <mackland@kth.se>
2018-03-29 00:05:11 -0700

    started adding main body of code to parser

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,3 +1,25 @@
 package parser;
 
+import java.io.*;
+import lexer.*;
+import symbols.*;
+import inter.*;
 
+public class Parser {
+	private Lexer lex;		//	lexical analyzer for this parser
+	private Token look;		//	lookahead tagen
+	Env top = null;			// top symbol table
+	int used = 0;			// storage used for declarations
+
+	public Parser(Lexer l) throws IOException {
+		lex = l;
+		move();
+	}
+
+	void move() throws IOException{}
+
+	void error(String s) throws IOException{}
+
+	void match(int t) throws IOException {}
+
+}
