2d98493501d9707305303aa2ea2e6c72b06b1390
Marcus Ackland <mackland@kth.se>
2018-03-31 11:35:02 -0700

    code for identifier

---
+++ src/inter/Id.java
@@ -0,0 +1,12 @@
+package inter;
+
+import lexer.*;
+import symbols.*;
+
+public class Id extends Expr {
+	public int offset;
+	public Id(Keyword id, Type t, int b){
+		super(id, p);
+		offset = b;
+	}
+}
