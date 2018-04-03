1e000ff68fbef29a2dd7be3f85bc8514869c8f61
Marcus Ackland <mackland@kth.se>
2018-04-02 22:29:04 -0700

    formatting

--- src/inter/Node.java
+++ src/inter/Node.java
@@ -3,26 +3,26 @@ package inter;
 import lexer.*;
 
 public class Node {
-	int lexline = 0;
+    int lexline = 0;
 
-	Node() {
-		lexline = Lexer.line;
-	}
-	
-	void error(String s) {
-		throw new Error("near line "+lexline+": "+s);
-	}
+    Node() {
+        lexline = Lexer.line;
+    }
+    
+    void error(String s) {
+        throw new Error("near line "+lexline+": "+s);
+    }
 
-	static int labels = 0;
-	public int newlabel() {
-		return ++labels;
-	}
+    static int labels = 0;
+    public int newlabel() {
+        return ++labels;
+    }
 
-	public void emitlabel(int i) {
-		System.out.println("L" + i + " : " );
-	}
+    public void emitlabel(int i) {
+        System.out.println("L" + i + " : " );
+    }
 
-	public void emit(String s) {
-		System.out.println("\t" + s);
-	}
+    public void emit(String s) {
+        System.out.println("\t" + s);
+    }
 }
