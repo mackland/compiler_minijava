b2337148081c2c561f07200f0cf30613707b6d15
Marcus Ackland <mackland@kth.se>
2018-04-02 22:28:51 -0700

    formatting

--- src/inter/Id.java
+++ src/inter/Id.java
@@ -4,9 +4,9 @@ import lexer.*;
 import symbols.*;
 
 public class Id extends Expr {
-	public int offset;
-	public Id(Keyword id, Type t, int b){
-		super(id, p);
-		offset = b;
-	}
+    public int offset;
+    public Id(Keyword id, Type t, int b){
+        super(id, p);
+        offset = b;
+    }
 }
--- src/inter/Op.java
+++ src/inter/Op.java
@@ -4,14 +4,14 @@ import lexer.*;
 import symbols.*;
 
 public class Op extends Expr {
-	public Op(Token tok, Type t) {
-		super(tok, t);
-	}
+    public Op(Token tok, Type t) {
+        super(tok, t);
+    }
 
-	public Expr reduce() {
-		Expr x = gen();
-		temp tmp = new Temp(type);
-		emit( t.toString() + " = " + x.toString() );
-		return tmp;
-	}
+    public Expr reduce() {
+        Expr x = gen();
+        temp tmp = new Temp(type);
+        emit( t.toString() + " = " + x.toString() );
+        return tmp;
+    }
 }
